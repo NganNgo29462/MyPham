eba217fd28ada09e6caa52f4a2324fe9a433d942
NganNgo29462 <168353470+NganNgo29462@users.noreply.github.com>
2024-08-28 10:35:26 +0700

    Add files via upload

---
+++ Site.Master.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Doan_thuchanhnghenghiep
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx");
+        }
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Home.aspx");
+        }
+        protected void btnCart_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Giohang.aspx");
+        }
+        protected void btnShowModal_Click(object sender, EventArgs e)
+        {
+            pnlModal.Style["display"] = "block";
+            modalBackground.Style["display"] = "block";
+        }
+        protected void btnCloseModal_Click(object sender, EventArgs e)
+        {
+            pnlModal.Style["display"] = "none";
+            modalBackground.Style["display"] = "none";
+        }
+
+    }
+}
+
